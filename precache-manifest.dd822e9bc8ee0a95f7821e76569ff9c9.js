b4f76d701f8d6b636e78d4bcf21475df36d00375
tennoken <tennoken@naver.com>
2020-05-27 13:52:31 +0900

    Updates

---
+++ precache-manifest.dd822e9bc8ee0a95f7821e76569ff9c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3622b4e2c8ceb250cc1ef0e786ee3a24",
+    "url": "/typescript-react-news-app/index.html"
+  },
+  {
+    "revision": "1be523e211ebe8592802",
+    "url": "/typescript-react-news-app/static/css/main.5ad3e9b7.chunk.css"
+  },
+  {
+    "revision": "fb60ac2740a7eaf009af",
+    "url": "/typescript-react-news-app/static/js/2.621b4252.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/typescript-react-news-app/static/js/2.621b4252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be523e211ebe8592802",
+    "url": "/typescript-react-news-app/static/js/main.bf229ef2.chunk.js"
+  },
+  {
+    "revision": "5c7e3d2988ecd705ad0d",
+    "url": "/typescript-react-news-app/static/js/runtime-main.09d22c5d.js"
+  }
+]);
