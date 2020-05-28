a0aa72055f94f2cd8c2636fd63261784ad4ff1cb
tennoken <tennoken@naver.com>
2020-05-28 12:03:14 +0900

    Updates

--- precache-manifest.0dc00fd408f9e0119d48857f8ad37f51.js
+++ precache-manifest.0dc00fd408f9e0119d48857f8ad37f51.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "00274388de88157ad08a160f2ec26b5b",
+    "revision": "dafce274ac4be334030b26fa607bbb26",
     "url": "/typescript-react-news-app/index.html"
   },
   {
-    "revision": "2c95a5734c5578c6f086",
+    "revision": "4c9bee967c2e1eedb0bf",
     "url": "/typescript-react-news-app/static/css/main.5ad3e9b7.chunk.css"
   },
   {
-    "revision": "c18a256ea08ea6d366f4",
-    "url": "/typescript-react-news-app/static/js/2.d1147ae7.chunk.js"
+    "revision": "fb60ac2740a7eaf009af",
+    "url": "/typescript-react-news-app/static/js/2.621b4252.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/typescript-react-news-app/static/js/2.d1147ae7.chunk.js.LICENSE.txt"
+    "url": "/typescript-react-news-app/static/js/2.621b4252.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2c95a5734c5578c6f086",
-    "url": "/typescript-react-news-app/static/js/main.aa7da19d.chunk.js"
+    "revision": "4c9bee967c2e1eedb0bf",
+    "url": "/typescript-react-news-app/static/js/main.8270989f.chunk.js"
   },
   {
     "revision": "5c7e3d2988ecd705ad0d",
