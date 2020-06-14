96ca7821c112e20657bc1948e30f43243053b433
tennoken <tennoken@naver.com>
2020-06-15 02:50:43 +0900

    add try & catch

--- src/api/news.ts
+++ src/api/news.ts
@@ -4,11 +4,15 @@ axios.defaults.baseURL =
   process.env.NODE_ENV === 'development' ? '/' : 'https://newsapi.org/v2';
 
 export async function getNewsApi(category: string) {
-  const receivedCategory = category === '' ? '' : `&category=${category}`;
-  const response = await axios.get(
-    `/top-headlines?country=kr&apiKey=c5d4a2f093234ce1ac7923d7d507de61${receivedCategory}`
-  );
-  return response.data.articles;
+  try {
+    const receivedCategory = category === '' ? '' : `&category=${category}`;
+    const response = await axios.get(
+      `/top-headlines?country=kr&apiKey=c5d4a2f093234ce1ac7923d7d507de61${receivedCategory}`
+    );
+    return response.data.articles;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export type NewsData = {
